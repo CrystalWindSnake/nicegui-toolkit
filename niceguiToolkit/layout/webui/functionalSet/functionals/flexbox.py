1f165bef3096c44eb0c2853bad65dd7cdfc9f6f7
CrystalWindSnake <568166495@qq.com>
2023-11-03 03:18:55 +0800

    gap

--- niceguiToolkit/layout/webui/functionalSet/functionals/flexbox.py
+++ niceguiToolkit/layout/webui/functionalSet/functionals/flexbox.py
@@ -6,7 +6,7 @@ def isShow_fn(context: T_BuilderContext):
     return context.flex_info.isFlex
 
 
-def build_fn(context: T_BuilderContext):
+def build_direction(context: T_BuilderContext):
     style_name = "flex-direction"
 
     def onchange(e):
@@ -19,6 +19,29 @@ def build_fn(context: T_BuilderContext):
         ui.radio(
             {"row": "横向", "column": "竖向"}, value=init_value, on_change=onchange
         ).classes("flex")
+
+
+def build_gap(context: T_BuilderContext):
+    style_name = "gap"
+
+    def onchange(e):
+        value = e.value
+        if value:
+            context.apply_styles({style_name: f"{e.value}rem"})
+        else:
+            context.remove_styles(style_name)
+
+    init_value = context.element._style.get(style_name, None)
+
+    with ui.row().classes("flex-center"):
+        ui.number(
+            "元素间空隙", min=0, value=init_value, step=0.1, on_change=onchange
+        ).classes("flex")
+
+
+def build_fn(context: T_BuilderContext):
+    build_direction(context)
+    build_gap(context)
 
 
 def _get_builder():
