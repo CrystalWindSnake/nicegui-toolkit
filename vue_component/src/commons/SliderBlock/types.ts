88f51151401a37ec060ec10d84b5aa2f5b0d4fc9
CrystalWindSnake <568166495@qq.com>
2023-11-17 00:07:02 +0800

    slider block

---
+++ vue_component/src/commons/SliderBlock/types.ts
@@ -0,0 +1,7 @@
+
+
+
+export type TBlockType = 'padding-up' | 'padding-right' | 'padding-bottom' | 'padding-left' | 'margin-up' | 'margin-right' | 'margin-bottom' | 'margin-left'
+
+
+
--- vue_component/src/main.ts
+++ vue_component/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Temp from './Temp.vue'
 import 'uno.css'
 
-createApp(App).mount('#app')
+createApp(Temp).mount('#app')
