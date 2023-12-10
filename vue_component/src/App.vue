802761d7e58ae885c88a64116f74bcfb4fdd4aa5
CrystalWindSnake <568166495@qq.com>
2023-12-11 00:17:13 +0800

    test

--- vue_component/src/App.vue
+++ vue_component/src/App.vue
@@ -1,28 +1,14 @@
 <script setup lang="ts">
 import DataEditor from "@/panels/Spacing/DataEditor.vue";
+import App from "@/temps/App.vue";
 import { ref } from "vue";
-
-const text = ref("");
-
-const oninput = (event) => {
-  console.log("oninput run");
-
-  text.value = event.target.value;
-};
-
-function onclickbtn() {
-  text.value = "on btn";
-}
 </script>
 
 <template>
   <div class="box">
     <!-- <DataEditor></DataEditor> -->
 
-    <input :value="text" @change="oninput" />
-    <span>{{ text }}</span>
-
-    <button @click="onclickbtn">change</button>
+    <App></App>
   </div>
 </template>
 
