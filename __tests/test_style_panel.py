540ba9ddc73606211cca01bc2f27c3d69af6b25e
CrystalWindSnake <568166495@qq.com>
2024-10-09 19:54:45 +0800

    tests

--- __tests/test_style_panel.py
+++ __tests/test_style_panel.py
@@ -41,7 +41,7 @@ def test_layout_display_value(browser: BrowserManager, page_path: str):
     actions.expect_contains_class(get_by_text="flex", class_name="arco-tag-checked")
 
 
-def test_set_size_width(browser: BrowserManager, page_path: str):
+def test_set_style(browser: BrowserManager, page_path: str):
     @ui.page(page_path)
     def _():
         inject_layout_tool(language_locale="en")
@@ -63,3 +63,43 @@ def test_set_size_width(browser: BrowserManager, page_path: str):
     )
 
     expect(page.locator(".test-input")).to_have_value("width: 500px;")
+
+
+def test_verify_preset_style(browser: BrowserManager, page_path: str):
+    @ui.page(page_path)
+    def _():
+        inject_layout_tool(language_locale="en")
+        ui.label("Hello World").style("width: 500px;")
+
+    page = browser.open(page_path)
+    actions = PageActions(page)
+
+    actions.open_style_panel("Hello World", auto_move_away=False)
+
+    actions.click_by_text("size")
+
+    expect(page.get_by_text("500")).to_be_visible()
+
+
+def test_reset_style(browser: BrowserManager, page_path: str):
+    @ui.page(page_path)
+    def _():
+        inject_layout_tool(language_locale="en")
+        ui.label("Hello World").style("width: 500px;")
+        ui.element("input").classes("test-input")
+
+    page = browser.open(page_path)
+    actions = PageActions(page)
+
+    actions.open_style_panel("Hello World", auto_move_away=False)
+
+    actions.click_by_text("size")
+
+    actions.click_by_text("Width")
+    actions.click_by_text("Reset")
+
+    page.get_by_text("Hello World", exact=True).evaluate(
+        r'(element) => {document.querySelector(".test-input").value = element.getAttribute("style");}'
+    )
+
+    expect(page.locator(".test-input")).to_have_value("")
